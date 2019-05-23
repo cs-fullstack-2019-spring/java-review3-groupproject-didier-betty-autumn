05d46d99d4f4548dae908e7075e6b1be55efd46d
student <bettyjcawthon@gmail.com>
2019-05-23 14:37:15 -0500

    create

--- src/com/company/Message.java
+++ src/com/company/Message.java
@@ -14,8 +14,8 @@ public class Message {
 
     //    setting up message properties
     private int mailID;
-    private String messageSubject;
-    private String messageBody;
+    private String subject;
+    private String body;
     private Boolean isImportant;
     private int fromUserID;
     private int sentUserID;
@@ -24,25 +24,25 @@ public class Message {
 
     public static void displayResults(ResultSet rs)throws SQLException {
         while (rs.next()) {
-            System.out.print("mailID: " + rs.getString(1));
-            System.out.print("messageSubject: " + rs.getString(2));
-            System.out.print(" messageBody: " + rs.getString(3));
-            System.out.println(" fromUserID: " + rs.getString(4));
-            System.out.println(" sentUserID: " + rs.getString(5));
-            System.out.println(" dateTimeSent: " + rs.getString(6));
+            System.out.print("mailid: " + rs.getString(1));
+            System.out.print("subject: " + rs.getString(2));
+            System.out.print(" body: " + rs.getString(3));
+            System.out.println(" fromuserid: " + rs.getString(4));
+            System.out.println(" sentuserid: " + rs.getString(5));
+            System.out.println(" datetimesent: " + rs.getString(6));
         }
     }
 
     //    Create Message records from the database
-    public static void createMessage(int messageID, String messageSubject, String messageBody, int fromUserID, int fromSentID, int dateTimeSent){
+    public static void createMessage(int mailID, String subject, String body, int fromUserID, int fromSentID, int dateTimeSent){
         String SQL =
-                "INSERT INTO ccmail (messageID, messageSubject, messageBody, fromUserID, sentUserID, dateTimeSent) "+
+                "INSERT INTO ccmail (mailid, subject, body, fromUserID, sentUserID, dateTimeSent) "+
                         " VALUES (?,?,?,?,?,?)";
         try (Connection conn = connect();
              PreparedStatement pstmt = conn.prepareStatement(SQL)){
-            pstmt.setString(1,messageID);
-            pstmt.setString(2,messageSubject);
-            pstmt.setString(3,messageBody);
+            pstmt.setString(1,mailID);
+            pstmt.setString(2,subject);
+            pstmt.setString(3,body);
             pstmt.setInt(4,fromUserID);
             pstmt.setInt(5,fromSentID);
             pstmt.setInt(6,dateTimeSent);
