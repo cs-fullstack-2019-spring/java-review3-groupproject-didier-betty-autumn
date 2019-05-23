fc2742f03ab0cfc7f4ea908c358c894e49203504
marcus110379 <marcusjmadden@gmail.com>
2019-05-23 12:10:37 -0500

    DONE

--- src/com/company/User.java
+++ src/com/company/User.java
@@ -1,4 +1,46 @@
 package com.company;
+import java.sql.*;
 
 public class User {
+    private static String url = "jdbc:postgresql://138.197.107.95:5432/group2";
+    private static String user = "student";
+    private static String password = "C0d3Cr3w";
+
+    private static Connection connect() throws SQLException {
+        Connection conn = null;
+        try{
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected!");
+        }
+        catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }
+        return conn;
+    }
+
+    private void newUser(String username, String password) throws SQLException{
+        String insertSQL = "INSERT INTO usertable (username,password) VALUES(?,?)";
+        Connection conn = null;
+        PreparedStatement createnewuser = null;
+
+        try{
+            conn = connect();
+            createnewuser = conn.prepareStatement(insertSQL);
+            createnewuser.setString(1,username);
+            createnewuser.setString(2,password);
+            int rs = createnewuser.executeUpdate();
+
+
+        }catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        finally {
+            if(createnewuser != null){
+                createnewuser.close();
+            }
+            if(conn != null){
+                conn.close();
+            }
+        }
+    }
 }
